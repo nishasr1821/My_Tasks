8cdaa39a1a95129f1024e62b2ae8bc4e22974c70
nishasr1821 <168284818+nishasr1821@users.noreply.github.com>
2024-05-07 22:51:44 +0530

    Add files via upload
    Week2.day1 Handson

---
+++ selenium/Loginpage.java
@@ -0,0 +1,15 @@
+package week2.selenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Loginpage {
+	public static void main(String[] args) {
+		 ChromeDriver driver=new ChromeDriver(); 
+		 driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+		} 
+
+}
